868ab392e333bba318e6e171841cabed66232cda
seunwoo <leemoney93@naver.com>
2021-01-25 08:54:37 +0900

    first commit

---
+++ src/main/java/guru/spring/sfgspringpet/SfgSpringPetApplication.java
@@ -0,0 +1,13 @@
+package guru.spring.sfgspringpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgSpringPetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgSpringPetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/spring/sfgspringpet/SfgSpringPetApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.spring.sfgspringpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgSpringPetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
